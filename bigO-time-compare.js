0303a31e3c25b48f6dbed06e75430b0447b904b3
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-14 14:01:41 +0700

    time compare with different algorithm

---
+++ bigO-time-compare.js
@@ -0,0 +1,21 @@
+//slow one
+// function addUpTo(n) {
+//     let total = 0;
+//     for (let i = 1; i <= n; i++) {
+//       total += i;
+//     }
+//     return total;
+//   }
+  
+  //fast one
+  function addUpTo(n) {
+    return n * (n + 1) / 2;
+  }
+  
+  var time1 = performance.now();
+  let answer=addUpTo(1000000000);
+  var time2 = performance.now();
+  console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+
+
+  console.log(answer)
