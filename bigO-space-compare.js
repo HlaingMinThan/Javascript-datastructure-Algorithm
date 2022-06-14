f137f073dcabb9bfb34f3b3a22984a7ed040d202
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-14 15:53:35 +0700

    space complexity

---
+++ bigO-space-compare.js
@@ -0,0 +1,5 @@
+//space complexity
+
+// primitives are constant space
+//but string is abit different, it can say O(n) => 50chars is 50 more space than single char
+//reference type (array and object) ,it can say O(n) => if one array have 2 indexes and another one have 4indexes.it can another array is twice space more than first array
