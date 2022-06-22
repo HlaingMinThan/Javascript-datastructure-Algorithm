8f7474d737b53ab96316ea0e7b80ab0ba22bfbe8
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-22 23:06:39 +0700

    frequency counter pattern example

--- frequency-counter.js
+++ frequency-counter.js
@@ -2,23 +2,55 @@
 //same([2,1,4],[16,1]) -> false
 
 //quick solve
+// function same(arr1,arr2){
+//     let answer;
+//     // if two array have different length,always the ans is false;
+//     if(arr1.length !== arr2.length){
+//             answer=false;
+//             return answer;
+//     }
+//     for(let i=0;i<arr1.length;i++){
+//         let num=arr1[i];
+//         if(!arr2.includes(num**2)){
+//             answer=false
+//             break;
+//         }else{
+//             answer=true
+//         }
+//     }
+//     return answer;
+// }
+
+// console.log(same([1,2,2,3],[9,1,4,4]));
+
+//frequency counter pattern
+
+function getFrequency(arr){
+    let frequency={};
+    for(let value of arr){
+        frequency[value]=(frequency[value]||0)+1;
+    }
+    return frequency;
+}
+
 function same(arr1,arr2){
     let answer;
-    // if two array have different length,always the ans is false;
     if(arr1.length !== arr2.length){
             answer=false;
             return answer;
     }
-    for(let i=0;i<arr1.length;i++){
-        let num=arr1[i];
-        if(!arr2.includes(num**2)){
-            answer=false
-            break;
-        }else{
-            answer=true
+    let frequency1=getFrequency(arr1);
+    let frequency2=getFrequency(arr2);
+
+    for(let key in frequency1){
+        if(!(key**2 in frequency2)){
+            return false;
+        }
+        if(frequency1[key]!==frequency2[key**2]){
+            return false;
         }
     }
-    return answer;
+    return true;
 }
 
 console.log(same([1,2,2,3],[9,1,4,4]));
