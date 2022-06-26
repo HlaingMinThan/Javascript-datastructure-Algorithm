90871d5f22815905296f55dd673dd245a01f5d6e
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-26 21:35:46 +0700

    two pointers first example done

---
+++ two-pointer.js
@@ -0,0 +1,24 @@
+//create a function which accept sorted numbers of array and return the first pair of sum two number which is going to be zero.
+
+
+//sumZero([-2,-1,0,1,2]) => [-2,2]
+//sumZero([-3,-1,0,4,7]) => undefined
+
+function sumZero(arr){
+    let left=0;
+    let right=arr.length-1;
+
+    while(left < right){
+        let sum=arr[left]+arr[right]
+        if(sum===0){
+            return [arr[left],arr[right]]
+        }else if(sum < 0){
+            left++;
+        }else{
+            right--;
+        }
+    }
+}
+
+
+console.log(sumZero([-3,-1,0,3,7]));
