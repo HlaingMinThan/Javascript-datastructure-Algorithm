40dee9207888f1550619ba5a98808d2505175bf6
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-07-09 11:29:29 +0700

    areThereDuplicate problem solved

--- problems.js
+++ problems.js
@@ -11,23 +11,44 @@ function getFrequency(n){
 }
 
 
-function sameFrequency(n1,n2){
-    if(n1.toString().length !== n2.toString().length){
-        return false;
-    }
-    let f1=getFrequency(n1);
-    let f2=getFrequency(n2);
+// function sameFrequency(n1,n2){
+//     if(n1.toString().length !== n2.toString().length){
+//         return false;
+//     }
+//     let f1=getFrequency(n1);
+//     let f2=getFrequency(n2);
 
-    for(let key in f1){
-        if(!(key in f2)){
-            return false;
-        }
-        if(f1[key]!==f2[key]){
-            return false;
+//     for(let key in f1){
+//         if(!(key in f2)){
+//             return false;
+//         }
+//         if(f1[key]!==f2[key]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+// console.log(sameFrequency(12443,36144));
+
+
+function areThereDuplicates(...data) {
+    let arr=data;
+
+    let left=0;
+    let right=arr.length-1;
+
+    while(left < right){
+        if(arr[left]===arr[right]){
+            return true
+        }else if (arr[left] > arr[right]){
+                right --;
+        }else{
+            left ++;
         }
     }
-    return true;
+    return false;
 }
 
-
-console.log(sameFrequency(12443,36144));
+console.log(areThereDuplicates(1,2,7,4,7))
